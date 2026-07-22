89850ce376cda5303edf7a58529f6240f973f058
@Monserrat-Vil <monseroo777@gmail.com>
2026-07-22 08:00:06 -0600

    feat: add gitignore

--- schemas/user_profile_schema.py
+++ schemas/user_profile_schema.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class UserProfile:
-    preferred_degree: str
+    preferred_degrees: list[str]
     preferred_countries: list[str]
     preferred_fields: list[str]
     english_level: str
